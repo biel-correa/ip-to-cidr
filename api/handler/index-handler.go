bf999d97af23a820d3a6b8b1bb21cb455fc17528
biel-correa <gabriel@correa.dev.br>
2024-09-13 20:06:37 -0300

    feature: convert ip to cidr64

--- api/handler/index-handler.go
+++ api/handler/index-handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,7 +10,37 @@ import (
 
 func IndexHandler(c *gin.Context) {
 	var requestIp = c.ClientIP()
+	var ip = c.DefaultQuery("ip", "")
+
+	if ip == "" {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"ip": requestIp,
+		})
+		return
+	}
+
+	parsedIp := net.ParseIP(ip)
+	if parsedIp == nil {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"ip":    requestIp,
+			"error": "Invalid IP address",
+		})
+		return
+	}
+
+	cidrFormat := fmt.Sprintf("%s/64", ip)
+	_, cidr, _ := net.ParseCIDR(cidrFormat)
+
+	if cidr == nil {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"ip":    requestIp,
+			"error": "Could not parse to CIDR/64",
+		})
+		return
+	}
+
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"ip": requestIp,
+		"ip":   requestIp,
+		"cidr": cidr,
 	})
 }
