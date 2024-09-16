a5b96af0945074081221403c21dfb475020c1a31
biel-correa <gabriel@correa.dev.br>
2024-09-16 17:40:23 -0300

    reafact: separate js from html

---
+++ web/assets/js/index.js
@@ -0,0 +1,30 @@
+const lastSubnetMask = () => {
+  const urlParams = new URLSearchParams(window.location.search)
+  const subnetMask = urlParams.get('subnetMask')
+  return subnetMask <= 128 ? subnetMask : 32
+}
+
+const createSubnetMaskOptions = () => {
+  const subnetMask = $('#subnetMask')
+  for (let i = 0; i <= 128; i++) {
+    subnetMask.append($('<option>', {
+      value: i,
+      text: `/${i}`
+    }))
+  }
+
+  subnetMask.val(lastSubnetMask())
+}
+
+$(document).ready(() => {
+  createSubnetMaskOptions()
+
+  $('#useYourIp').click(() => {
+    $(idInput).val(clientIp)
+    $('#submit').click()
+  })
+
+  $('#copyResult').click(() => {
+    navigator.clipboard.writeText("{{ .cidr }}")
+  })
+})
